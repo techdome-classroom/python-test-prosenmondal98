4f47b9fb465c9686931d0cb39875cef8d65b04f7
prosenjit mondal <prosenjitmndal98@gmail.com>
2024-05-08 11:29:35 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -8,7 +8,6 @@ class Solution:
             for j in range(c):
                 if grid[i][j]=='W':
                     continue
-
                 #  top check
                 if i==0:
                     continue
