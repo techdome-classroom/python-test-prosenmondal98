c0ffb0ad3f672b7c13cabb10967ee89238645ddb
prosenjit mondal <prosenjitmndal98@gmail.com>
2024-05-08 11:10:02 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -14,7 +14,6 @@ def decode_message( s: str, p: str) -> bool:
                         else:
                                 return False
         
-                if n==len(s):
-                        return True
+                
   
         return False
