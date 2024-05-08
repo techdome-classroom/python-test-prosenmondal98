4a31cb4193bdc3a803ad10b2bfcf725994c80858
prosenjit mondal <prosenjitmndal98@gmail.com>
2024-05-08 11:16:29 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -3,16 +3,16 @@ def decode_message( s: str, p: str) -> bool:
 # write your code here
         n=0
         for i in range(len(s)):
-                for j in range(len(p)):
-                        if p[j]=='*':
-                                return True
-                        
-                        elif s[i]==p[j] or p[j]=='?':
-                                n+=1
-                                continue
-                        
-                        else:
-                                return False
+        for j in range(len(p)):
+                if p[j]=='*':
+                        return True
+                
+                elif s[i]==p[j] or p[j]=='?':
+                        n+=1
+                        continue
+                
+                else:
+                        return False
         
                 
   
