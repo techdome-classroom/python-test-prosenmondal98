9005ff544a1395bfa0825c86d3373eed1a64fde3
prosenjit mondal <prosenjitmndal98@gmail.com>
2024-05-08 11:27:04 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -13,7 +13,7 @@ class Solution:
                 if j==0:
                     pass
                 # right check
-                if 
+                if j==
                 # bottom check
 
                     
